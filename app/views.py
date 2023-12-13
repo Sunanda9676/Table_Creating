969ead095d075d4531899f4121cd7f58307648b6
Sunanda9676 <sunandacse523@gmail.com>
2023-12-13 19:39:44 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from app.models import *
 
 # Create your views here.
+def display_topics(request):
+    QLTO=Topic.objects.all()
+    d={'topics':QLTO}
+    return render(request,'display_topics.html',d)
+
+
+def display_webpage(request):
+    QLWO=Webpage.objects.all()
+    d={'webpage':QLWO}
+    return render(request,'display_webpage.html',d)
